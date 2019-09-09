7f97df356a4ca78b6eb143b9a2c462f928e5fbf3
Joao Boaventura <jboaventura83@gmail.com>
2019-09-09 19:07:46 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.          
         </p>
         <br />
-        <p>Its was altered!</p>
+        <p>Its was altered! One more time</p>
 
         <a
           className="App-link"
